645573e82598142aefea6ef0a669c454e85e1dd5
lucas.kim <lucas.kim@kakaocorp.com>
2019-06-21 19:08:50 +0900

    refactoring

--- tests/algo/test_als.py
+++ tests/algo/test_als.py
@@ -1,47 +1,14 @@
 # -*- coding: utf-8 -*-
-import os
 import unittest
-import tempfile
 from buffalo.algo.als import ALS
 from buffalo.algo.options import AlsOption
 from buffalo.misc.log import set_log_level
 from buffalo.data import MatrixMarketOptions
 
+from .base import TestBase
 
-class TestALS(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        if os.path.isfile('ml-100k/'):
-            raise RuntimeError('Cannot find the resource  on ./ml-100k directory, checkout: https://grouplens.org/datasets/movielens/100k/')
-        if not os.path.isfile('./ml-100k/main'):
-            with open('./ml-100k/main', 'w') as fout:
-                fout.write('%%MatrixMarket matrix coordinate integer general\n%\n%\n944 1682 80000\n')
-                with open('./ml-100k/u1.base') as fin:
-                    for line in fin:
-                        u, i, v, ts = line.strip().split('\t')
-                        fout.write('%s %s %s\n' % (u, i, v))
 
-            with open('./ml-100k/iid', 'w') as fout:
-                with open('./ml-100k/u.item', encoding='ISO-8859-1') as fin:
-                    for line in fin:
-                        fout.write('%s\n' % line.strip().split('|')[1].encode('utf8'))
-
-            with open('./ml-100k/uid', 'w') as fout:
-                for line in open('./ml-100k/u.user'):
-                    fout.write('%s\n' % line.strip())
-        cls.mm_path = './ml-100k/main'
-        cls.iid_path = './ml-100k/iid'
-        cls.uid_path = './ml-100k/uid'
-        cls.temp_files = []
-
-    @classmethod
-    def tearDownClass(cls):
-        os.remove(cls.mm_path)
-        os.remove(cls.uid_path)
-        os.remove(cls.iid_path)
-        for path in cls.temp_files:
-            os.remove(path)
-
+class TestALS(TestBase):
     def test0_get_default_option(self):
         AlsOption().get_default_option()
         self.assertTrue(True)
