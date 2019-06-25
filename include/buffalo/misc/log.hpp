0cdf16e6cafe7519917ecc67ab8a1861ad16b014
lucas.kim <lucas.kim@kakaocorp.com>
2019-06-25 21:27:58 +0900

    refactoring log

--- include/buffalo/misc/log.hpp
+++ include/buffalo/misc/log.hpp
@@ -10,42 +10,24 @@
 #define INFO(x, ...) logger_->info("[{}:{}] " x "\n", __FILENAME__, __LINE__, __VA_ARGS__);
 #define DEBUG(x, ...) logger_->debug("[{}:{}] " x "\n", __FILENAME__, __LINE__, __VA_ARGS__);
 #define WARN(x, ...) logger_->warn("[{}:{}] " x "\n", __FILENAME__, __LINE__, __VA_ARGS__);
+#define TRACE(x, ...) logger_->trace("[{}:{}] " x "\n", __FILENAME__, __LINE__, __VA_ARGS__);
 #define CRITICAL(x, ...) logger_->critical("[{}:{}] " x "\n", __FILENAME__, __LINE__, __VA_ARGS__);
 
 #define INFO0(x) logger_->info("[{}:{}] " x "\n", __FILENAME__, __LINE__);
 #define DEBUG0(x) logger_->debug("[{}:{}] " x "\n", __FILENAME__, __LINE__);
 #define WARN0(x) logger_->warn("[{}:{}] " x "\n", __FILENAME__, __LINE__);
+#define TRACE0(x) logger_->trace("[{}:{}] " x "\n", __FILENAME__, __LINE__);
 #define CRITICAL0(x) logger_->critical("[{}:{}] " x "\n", __FILENAME__, __LINE__);
-
 
 class BuffaloLogger
 {
 public:
-    BuffaloLogger() {
-        spdlog::set_pattern("[%^%-8l%$] [%Y-%m-%d %H:%M:%S] %v");
-        logger_ = spdlog::default_logger();
-        lvl_ = 1;
-    }
-
-    std::shared_ptr<spdlog::logger>& get_logger() {
-        return logger_;
-    }
-
-    void set_log_level(int level) {
-        lvl_ = level;
-        switch(level) {
-            case 0: spdlog::set_level(spdlog::level::off); break;
-            case 1: spdlog::set_level(spdlog::level::info); break;
-            case 2: spdlog::set_level(spdlog::level::debug); break;
-            default: spdlog::set_level(spdlog::level::trace); break;
-        }
-    }
-
-    int get_log_level() {
-        return lvl_;
-    }
+    BuffaloLogger();
+    std::shared_ptr<spdlog::logger>& get_logger();
+    void set_log_level(int level);
+    int get_log_level();
 
 private:
-    int lvl_;
+    static int global_logging_level_;
     std::shared_ptr<spdlog::logger> logger_;
 };
