510826339e78a3362ff27551b979e6aa730e63f5
lucas.kim <lucas.kim@kakaocorp.com>
2019-08-25 20:34:39 +0900

    refactoring benchmarks and added parallel

--- benchmark/test_performance.py
+++ benchmark/test_performance.py
@@ -1,29 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
-import json
 os.environ['OMP_NUM_THREADS'] = '1'
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
-import fire
-from tabulate import tabulate
 
+import fire
 from models import ImplicitLib, BuffaloLib, LightfmLib, QmfLib, PysparkLib
 
-
-def _get_elapsed_time(algo_name, database, lib, repeat, **options):
-    elapsed = []
-    mem_info = []
-    for i in range(repeat):
-        e, m = getattr(lib, algo_name)(database, **options)
-        elapsed.append(e)
-        mem_info.append(m)
-    elapsed = sum(elapsed) / len(elapsed)
-    mem_info = {'min': min([m['min'] for m in mem_info]),
-                'max': max([m['max'] for m in mem_info]),
-                'avg': sum([m['avg'] for m in mem_info]) / len(mem_info)}
-    return elapsed, mem_info
+from base import _get_elapsed_time, _print_table
 
 
-def _test1(algo_name, database, lib):
+def _performance(algo_name, database, lib):
     results = {}
     repeat = 3
     options = {'als': {'num_workers': 8,
@@ -58,39 +44,7 @@ def _test1(algo_name, database, lib):
     return results
 
 
-def benchmark1(algo_name, database, libs=['buffalo', 'implicit', 'lightfm', 'qmf', 'pyspark']):
-    assert database in ['ml100k', 'ml20m', 'kakao_reco_730m', 'kakao_brunch_12m']
-    assert algo_name in ['als', 'bpr']
-    if isinstance(libs, str):
-        libs = [libs]
-    if algo_name == 'als':
-        libs = [l for l in libs if l not in ['lightfm']]
-    elif algo_name == 'bpr':
-        libs = [l for l in libs if l not in ['pyspark']]
-    R = {'buffalo': BuffaloLib,
-         'implicit': ImplicitLib,
-         'lightfm': LightfmLib,
-         'qmf': QmfLib,
-         'pyspark': PysparkLib}
-    results = {l: _test1(algo_name, database, R[l]()) for l in libs}
-
-    for f in ['D', 'T', 'M', 'A', 'B']:
-        table = []
-        for lib_name in libs:
-            if lib_name not in results:
-                continue
-            data = sorted([(k, v) for k, v in results[lib_name].items()
-                        if k.startswith(f)],
-                        key=lambda x: (len(x[0]), x[0]))
-            rows = [v for _, v in data]
-            headers = ['method'] + [k for k, _ in data]
-            table.append([lib_name] + rows)
-        if table:
-            print(tabulate(table, headers=headers, tablefmt="github"))
-            print('')
-
-
-def _test2(algo_name, database, lib):
+def _memory(algo_name, database, lib):
     results = {}
     repeat = 3
     options = {'als': {'num_workers': 12,
@@ -117,30 +71,35 @@ def _test2(algo_name, database, lib):
     return results
 
 
-def benchmark2(algo_name, database, libs=['buffalo', 'implicit']):
+def performance(algo_name, database, libs=['buffalo', 'implicit', 'lightfm', 'qmf', 'pyspark']):
+    assert database in ['ml100k', 'ml20m', 'kakao_reco_730m', 'kakao_brunch_12m']
+    assert algo_name in ['als', 'bpr']
+    if isinstance(libs, str):
+        libs = [libs]
+    if algo_name == 'als':
+        libs = [l for l in libs if l not in ['lightfm']]
+    elif algo_name == 'bpr':
+        libs = [l for l in libs if l not in ['pyspark']]
+    R = {'buffalo': BuffaloLib,
+         'implicit': ImplicitLib,
+         'lightfm': LightfmLib,
+         'qmf': QmfLib,
+         'pyspark': PysparkLib}
+    results = {l: _performance(algo_name, database, R[l]()) for l in libs}
+    _print_table(results)
+
+
+def memory(algo_name, database, libs=['buffalo', 'implicit']):
     assert database in ['ml100k', 'ml20m', 'kakao_reco_730m', 'kakao_brunch_12m']
     assert algo_name in ['als', 'bpr']
     if isinstance(libs, str):
         libs = [libs]
     R = {'buffalo': BuffaloLib,
          'implicit': ImplicitLib}
-    results = {l: _test2(algo_name, database, R[l]()) for l in libs}
-
-    for f in ['M', 'A', 'B']:
-        table = []
-        for lib_name in libs:
-            data = sorted([(k, v) for k, v in results[lib_name].items()
-                        if k.startswith(f)],
-                        key=lambda x: (len(x[0]), x[0]))
-            rows = [v for _, v in data]
-            headers = ['method'] + [k for k, _ in data]
-            table.append([lib_name] + rows)
-        if table:
-            print(tabulate(table, headers=headers, tablefmt="github"))
-            print('')
-
+    results = {l: _memory(algo_name, database, R[l]()) for l in libs}
+    _print_table(results)
 
 
 if __name__ == '__main__':
-    fire.Fire({'benchmark1': benchmark1,
-               'benchmark2': benchmark2})
+    fire.Fire({'performance': performance,
+               'memory': memory})
