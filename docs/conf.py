933aea37b70b76bdf5aca55bd7fc65c281684644
lucas.kim <lucas.kim@kakaocorp.com>
2019-08-28 20:23:17 +0900

    remove not used theme

--- docs/conf.py
+++ docs/conf.py
@@ -52,8 +52,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-import sphinx_theme
-html_theme_path = [sphinx_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
