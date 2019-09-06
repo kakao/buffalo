60454da54e9dd046404805982084076ea03939ab
lucas.kim <lucas.kim@kakaocorp.com>
2019-09-06 10:49:01 +0900

    added missing files

---
+++ buffalo/misc/_log.pyx
@@ -0,0 +1,26 @@
+# cython: experimental_cpp_class_def=True, language_level=3
+# distutils: language=c++
+# -*- coding: utf-8 -*-
+
+
+cdef extern from "buffalo/misc/log.hpp":
+    cdef cppclass _BuffaloLogger "BuffaloLogger":
+        void set_log_level(int)
+        int get_log_level()
+
+
+cdef class PyBuffaloLog:
+    """CALS object holder"""
+    cdef _BuffaloLogger* obj
+
+    def __cinit__(self):
+        self.obj = new _BuffaloLogger()
+
+    def __dealloc__(self):
+        del self.obj
+
+    def set_log_level(self, lvl):
+        self.obj.set_log_level(lvl)
+
+    def get_log_level(self):
+        return self.obj.get_log_level()
