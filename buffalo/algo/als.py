095897094ff5679ba8515e852258efb25788a6b5
jason(윤지상) <jason.yoon@kakaocorp.com>
2019-09-26 14:36:22 +0900

    Hot fix jason (#68)
    * check data internal type
    
    * add log
    
    * add more assertion
    
    * check inited before declaring class instance

--- buffalo/algo/als.py
+++ buffalo/algo/als.py
@@ -39,11 +39,11 @@ class ALS(Algo, ALSOption, Evaluable, Serializable, Optimizable, TensorboardExte
 
         self.logger = log.get_logger('ALS')
         self.opt, self.opt_path = self.get_option(opt_path)
-        self.obj = CuALS() if self.opt.accelerator else CyALS()
         if self.opt.accelerator and not inited_CUALS:
             self.logger.error("ImportError CuALS, no cuda library exists.")
             raise RuntimeError()
         assert self.obj.init(bytes(self.opt_path, 'utf-8')), 'cannot parse option file: %s' % opt_path
+
         self.data = None
         data = kwargs.get('data')
         data_opt = self.opt.get('data_opt')
