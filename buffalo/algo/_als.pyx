a88f32c70b45aea585e33cedcb621f28d3008173
Nitin-2001 <70697349+Nitin-2001@users.noreply.github.com>
2020-10-05 20:15:37 +0530

    Update

--- buffalo/algo/_als.pyx
+++ buffalo/algo/_als.pyx
@@ -6,10 +6,8 @@ from libcpp cimport bool
 from libcpp.pair cimport pair
 from libcpp.string cimport string
 from libc.stdint cimport int64_t, int32_t
-
 import numpy as np
 cimport numpy as np
-
 
 cdef extern from "buffalo/algo_impl/als/als.hpp" namespace "als":
     cdef cppclass CALS:
@@ -24,31 +22,24 @@ cdef extern from "buffalo/algo_impl/als/als.hpp" namespace "als":
                                             int32_t*,
                                             float*,
                                             int) nogil except +
-
-
 cdef class CyALS:
     """CALS object holder"""
     cdef CALS* obj  # C-ALS object
-
+    
     def __cinit__(self):
         self.obj = new CALS()
-
     def __dealloc__(self):
         self.obj.release()
         del self.obj
-
     def init(self, option_path):
         return self.obj.init(option_path)
-
     def initialize_model(self,
                          np.ndarray[np.float32_t, ndim=2] P,
                          np.ndarray[np.float32_t, ndim=2] Q):
         self.obj.initialize_model(&P[0, 0], P.shape[0],
                                   &Q[0, 0], Q.shape[0])
-
     def precompute(self, axis):
         self.obj.precompute(axis)
-
     @cython.boundscheck(False)
     @cython.wraparound(False)
     def partial_update(self,
