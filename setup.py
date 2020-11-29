d806b115dbc1aad4ca293224319d316b5e50fcb4
lucas.kim <lucas.kim@kakaocorp.com>
2020-11-29 17:44:32 +0900

    version up

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ n2_shared_object = n2.__file__
 
 MAJOR = 1
 MINOR = 2
-MICRO = 0
+MICRO = 1
 Release = True
 STAGE = {True: '', False: 'b'}.get(Release)
 VERSION = f'{MAJOR}.{MINOR}.{MICRO}{STAGE}'
