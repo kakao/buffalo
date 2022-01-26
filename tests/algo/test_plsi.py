7a51f24a91823b77563cb9ec7d40c6d591ad6b48
dante.l <dante.l@kakaocorp.com>
2022-01-26 11:43:38 +0900

    add inheritance test

--- tests/algo/test_plsi.py
+++ tests/algo/test_plsi.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
+import os
 import unittest
 
+import numpy as np
+
+import buffalo.data
 from buffalo.misc import aux
 from buffalo.algo.plsi import PLSI
 from buffalo.algo.options import PLSIOption
+from buffalo.data.mm import MatrixMarketOptions
 
 from .base import TestBase
 
 
 class TestPLSI(TestBase):
-
     def test00_get_default_option(self):
         PLSIOption().get_default_option()
         self.assertTrue(True)
@@ -60,6 +64,41 @@ class TestPLSI(TestBase):
         opt = PLSIOption().get_default_option()
         opt.d = 10
         self._test9_compact_serialization(PLSI, opt)
+
+    def test10_inheritance(self):
+        uid1 = np.arange(0, 100).astype(str)
+        iid1 = np.arange(200, 300).astype(str)
+        main1 = np.random.binomial(1, 0.1, size=(100, 100))
+
+        data_opt1 = MatrixMarketOptions().get_default_option()
+        data_opt1.input = aux.Option({'main': main1, 'uid': uid1, 'iid': iid1})
+        data1 = buffalo.data.load(data_opt1)
+        data1.create()
+        plsi_opt1 = PLSIOption().get_default_option()
+        model1 = PLSI(plsi_opt1, data=data1)
+        model1.initialize()
+        model1.train()
+        model1.save('inherit_model.plsi')
+        os.remove('mm.h5py')
+
+        uid2 = np.arange(80, 150).astype(str)
+        iid2 = np.arange(280, 350).astype(str)
+        main2 = np.random.binomial(1, 0.1, size=(70, 70))
+        data_opt2 = MatrixMarketOptions().get_default_option()
+        data_opt2.input = aux.Option({'main': main2, 'uid': uid2, 'iid': iid2})
+        data2 = buffalo.data.load(data_opt2)
+        data2.create()
+        plsi_opt2 = PLSIOption().get_default_option()
+        plsi_opt2.inherit_opt = aux.Option({'model_path': 'inherit_model.plsi', 'inherit_user': True, 'inherit_item': True})  # noqa: E501
+        model2 = PLSI(plsi_opt2, data=data2)
+        model2.initialize()
+        os.remove('mm.h5py')
+        os.remove('inherit_model.plsi')
+
+        prev_p, curr_p = model1.P[-20:], model2.P[:20]
+        prev_q, curr_q = model1.Q[-20:], model2.Q[:20]
+        self.assertTrue(np.allclose(prev_p, curr_p))
+        self.assertTrue(np.allclose(prev_q, curr_q))
 
 
 if __name__ == '__main__':
