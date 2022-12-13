c85e1d948c45f77ba1e87fb1818dd2a1caf0d7bf
dante.l(이수현)/kakao <dante.l@kakaocorp.com>
2022-12-13 13:12:44 +0900

    Apply pyproject (#167)

--- build.py
+++ build.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
-"""Buffalo
-"""
-DOCLINES = __doc__.split("\n")
 
 import os
-import sys
 import platform
 import sysconfig
-import subprocess
 
 import numpy as np
-from setuptools import setup, Extension
+from setuptools import Extension
+from Cython.Build import cythonize
 
-from cuda_setup import CUDA, build_ext
-
-
-# TODO: Python3 Support
-if sys.version_info[:3] < (3, 6):
-    raise RuntimeError("Python version 3.6 or later required.")
+from cuda_build import CUDA, build_ext
 
 assert platform.system() == 'Linux'  # TODO: MacOS
 
@@ -27,8 +18,10 @@ MICRO = 2
 Release = True
 STAGE = {True: '', False: 'b'}.get(Release)
 VERSION = f'{MAJOR}.{MINOR}.{MICRO}{STAGE}'
-STATUS = {False: 'Development Status :: 4 - Beta',
-          True: 'Development Status :: 5 - Production/Stable'}
+STATUS = {
+    False: 'Development Status :: 4 - Beta',
+    True: 'Development Status :: 5 - Production/Stable'
+}
 
 CLASSIFIERS = """{status}
 Programming Language :: C++
@@ -42,10 +35,7 @@ License :: OSI Approved :: Apache Software License""".format(status=STATUS.get(R
 CLIB_DIR = os.path.join(sysconfig.get_path('purelib'), 'buffalo')
 numpy_include_dirs = np.get_include()
 LIBRARY_DIRS = [CLIB_DIR]
-EXTRA_INCLUDE_DIRS = [numpy_include_dirs,
-                      '3rd/json11',
-                      '3rd/spdlog/include',
-                      '3rd/eigen3']
+EXTRA_INCLUDE_DIRS = [numpy_include_dirs, '3rd/json11', '3rd/spdlog/include', '3rd/eigen3']
 
 
 def get_extend_compile_flags():
@@ -165,39 +155,7 @@ else:
     print("Failed to find CUDA toolkit. Building without GPU acceleration.")
 
 
-# Return the git revision as a string
-def git_version():
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = "Unknown"
-
-    return GIT_REVISION
-
-
-def write_version_py(filename='buffalo/version.py'):
-    cnt = """
-short_version = '%(version)s'
-git_revision = '%(git_revision)s'
-"""
-    GIT_REVISION = git_version()
-    with open(filename, 'w') as fout:
-        fout.write(cnt % {'version': VERSION,
-                          'git_revision': GIT_REVISION})
-
-
-class BuildExtension(build_ext, object):
+class BuildExtension(build_ext):
     def run(self):
         for ext in self.extensions:
             if hasattr(ext, 'extension_type') and ext.extension_type == 'cmake':
@@ -224,48 +182,15 @@ class BuildExtension(build_ext, object):
         os.chdir(str(cwd))
 
 
-def setup_package():
-    write_version_py()
+def build(kwargs):
     cmdclass = {
         'build_ext': BuildExtension
     }
-
-    with open('requirements.txt', 'r') as fin:
-        install_requires = [line.strip() for line in fin]
-
-    metadata = dict(
-        name='buffalo',
-        maintainer="lucas kwangseob kim",
-        maintainer_email="recoteam@kakaocorp.com",
-        description=DOCLINES[0],
-        long_description="\n".join(DOCLINES[2:]),
-        url="https://github.com/kakao/buffalo",
-        download_url="https://github.com/kakao/buffalo/releases",
-        include_package_data=False,
-        license='Apache2',
-        packages=['buffalo/algo/',
-                  'buffalo/algo/cuda',
-                  'buffalo/data/',
-                  'buffalo/evaluate/',
-                  'buffalo/parallel/',
-                  'buffalo/misc/',
-                  'buffalo/'],
-        cmdclass=cmdclass,
-        classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
-        platforms=['Linux', 'Mac OSX', 'Unix'],
-        ext_modules=extensions,
-        entry_points={
-            'console_scripts': [
-                'Buffalo = buffalo.cli:_cli_buffalo',
-            ]
-        },
-        python_requires='>=3.6',
-        install_requires=install_requires,
+    kwargs.update(
+        dict(
+            cmdclass=cmdclass,
+            ext_modules=cythonize(extensions),
+            platforms=['Linux'],
+            zip_safe=False,
+        )
     )
-
-    metadata['version'] = VERSION
-    setup(**metadata)
-
-
-if __name__ == '__main__':
-    setup_package()
