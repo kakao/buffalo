92ab98c72f7f4453c7da4a192031a64f53cb6f5a
iggy <iggy.ll@kakaocorp.com>
2022-12-28 21:06:05 +0900

    .

--- build_extension.py
+++ build_extension.py
@@ -8,6 +8,7 @@ from setuptools import Extension
 
 from cuda_extension import CUDA, build_ext
 
+
 MAJOR = 1
 MINOR = 2
 MICRO = 2
@@ -173,16 +174,17 @@ class BuildExtension(build_ext):
         ]
 
         if platform.system() == 'Darwin':
-            print("to build for MacOS, gcc-11 version must be installed and set(for m1 support)")
-            os.environ["CC"] = 'gcc-11'
-            os.environ["CXX"] = 'g++-11'
-            cmake_args += ['-DCMAKE_C_COMPILER=gcc-11', '-DCMAKE_CXX_COMPILER=g++-11']
+            cmake_args += [
+                '-DCMAKE_C_COMPILER=gcc-11',
+                '-DCMAKE_CXX_COMPILER=g++-11',
+            ]
         build_args = []
 
         os.chdir(self.build_temp)
         self.spawn(['cmake', str(cwd)] + cmake_args)
         if not self.dry_run:
-            self.spawn(['cmake', '--build', '.', '-j 4'] + build_args)
+            cpu_count = os.cpu_count()
+            self.spawn(['cmake', '--build', '.', '-j %d' % cpu_count] + build_args)
         os.chdir(str(cwd))
 
 
