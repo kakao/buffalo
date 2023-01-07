489667a4400207bc04f937ad7f58f777d025959b
Hyunsung Lee <ita9naiwa@gmail.com>
2023-01-07 16:45:04 +0900

    static lint

--- buffalo/__init__.py
+++ buffalo/__init__.py
@@ -1,2 +1,3 @@
 import importlib.metadata
+
 __version__ = importlib.metadata.version('buffalo')
--- buffalo/algo/__init__.py
+++ buffalo/algo/__init__.py
@@ -2,8 +2,15 @@ from .als import ALS, inited_CUALS
 from .base import Algo
 from .bpr import BPRMF, inited_CUBPR
 from .cfr import CFR
+from .options import (
+    AlgoOption,
+    ALSOption,
+    BPRMFOption,
+    CFROption,
+    PLSIOption,
+    W2VOption,
+    WARPOption,
+)
 from .plsi import PLSI
 from .w2v import W2V
 from .warp import WARP
-
-from .options import AlgoOption, ALSOption, BPRMFOption, CFROption, WARPOption, PLSIOption, W2VOption
